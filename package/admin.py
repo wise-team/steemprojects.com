7ca9b4151fb1b1d01ae56652d05dd160fabc84fe
Krzysztof Szumny <noisy.pl@gmail.com>
2017-09-01 06:08:07 +0200

    announcement_post added to admin

--- package/admin.py
+++ package/admin.py
@@ -20,7 +20,7 @@ class PackageAdmin(VersionAdmin):
     ]
     fieldsets = (
         (None, {
-            "fields": ("title", "url", "description", "created_by", "status", "slug", "category", "pypi_url", "repo_url", "usage", "added_by", "last_modified_by",)
+            "fields": ("title", "url", "description", "announcement_post", "created_by", "status", "slug", "category", "pypi_url", "repo_url", "usage", "added_by", "last_modified_by",)
         }),
         ("Pulled data", {
             "classes": ("collapse",),
