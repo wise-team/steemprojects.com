1d015ec4baa874636a483bb6d5d110aabca6a0e2
Krzysztof Szumny <noisy.pl@gmail.com>
2017-10-20 16:26:39 +0200

    Let's use scss syntax instead sass

--- bin/sass_watch.sh
+++ bin/sass_watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # watch sass files and generate new css once modified
 # usage from project root directory: ./bin/sass_watch.sh
-sass --watch static/sass:static/css
+sass --watch static/scss:static/css
