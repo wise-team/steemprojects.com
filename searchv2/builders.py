e5d6d2daff12782e150eb6c6077f57315a28448b
Krzysztof Szumny <noisy.pl@gmail.com>
2017-11-17 18:20:21 +0100

    Drafts should be also in search results

--- searchv2/builders.py
+++ searchv2/builders.py
@@ -18,95 +18,99 @@ def build_1(print_out=False):
     year_delta = timedelta(365)
 
     SearchV2.objects.all().delete()
-    published_projects = Project.objects.published()
 
-    if not published_projects.exists():
+    if not Project.objects.exists():
         return
 
-    for project in published_projects:
+    for project_type, projects in [
+        ('package', Project.objects.published()),
+        ('draft', Project.objects.drafts())
+    ]:
 
-        obj, created = SearchV2.objects.get_or_create(
-            item_type="package",
-            slug=project.slug,
-        )
-        obj.slug_no_prefix = remove_prefix(project.slug)
-        obj.clean_title = clean_title(remove_prefix(project.slug))
-        obj.title = project.name
-        obj.title_no_prefix = remove_prefix(project.name)
-        obj.description = project.description
-        obj.category = project.category.title
-        obj.absolute_url = project.get_absolute_url()
-        obj.repo_watchers = project.repo_watchers
-        obj.repo_forks = project.repo_forks
-        obj.pypi_downloads = project.pypi_downloads
-        obj.usage = project.usage.count()
-        obj.participants = project.participants
+        for project in projects:
 
-        optional_save = False
-        try:
-            obj.last_committed = project.last_updated()
-            optional_save = True
-        except Commit.DoesNotExist:
-            pass
+            obj, created = SearchV2.objects.get_or_create(
+                item_type=project_type,
+                slug=project.slug,
+            )
+            obj.slug_no_prefix = remove_prefix(project.slug)
+            obj.clean_title = clean_title(remove_prefix(project.slug))
+            obj.title = project.name
+            obj.title_no_prefix = remove_prefix(project.name)
+            obj.description = project.description
+            obj.category = project.category.title
+            obj.absolute_url = project.get_absolute_url()
+            obj.repo_watchers = project.repo_watchers
+            obj.repo_forks = project.repo_forks
+            obj.pypi_downloads = project.pypi_downloads
+            obj.usage = project.usage.count()
+            obj.participants = project.participants
 
-        last_released = project.last_released()
-        if last_released and last_released.upload_time:
-            obj.last_released = last_released.upload_time
-            optional_save = True
+            optional_save = False
+            try:
+                obj.last_committed = project.last_updated()
+                optional_save = True
+            except Commit.DoesNotExist:
+                pass
 
-        if optional_save:
-            obj.save()
+            last_released = project.last_released()
+            if last_released and last_released.upload_time:
+                obj.last_released = last_released.upload_time
+                optional_save = True
 
-        # Weighting part
-        # Weighting part
-        # Weighting part
-        weight = 0
-        optional_save = False
+            if optional_save:
+                obj.save()
 
-        # Read the docs!
-        rtfd_url = "http://readthedocs.org/api/v1/build/{0}/".format(obj.slug)
-        r = requests.get(rtfd_url)
-        if r.status_code == 200:
-            data = json.loads(r.content)
-            if data['meta']['total_count']:
-                weight += 20
+            # Weighting part
 
-        if obj.description.strip():
-            weight += 20
+            if project_type != 'draft':
+                weight = 0
+                optional_save = False
 
-        if obj.repo_watchers:
-            weight += min(obj.repo_watchers, 20)
+                # Read the docs!
+                rtfd_url = "http://readthedocs.org/api/v1/build/{0}/".format(obj.slug)
+                r = requests.get(rtfd_url)
+                if r.status_code == 200:
+                    data = json.loads(r.content)
+                    if data['meta']['total_count']:
+                        weight += 20
 
-        if obj.repo_forks:
-            weight += min(obj.repo_forks, 20)
+                if obj.description.strip():
+                    weight += 20
 
-        if obj.pypi_downloads:
-            weight += min(obj.pypi_downloads / 1000, 20)
+                if obj.repo_watchers:
+                    weight += min(obj.repo_watchers, 20)
 
-        if obj.usage:
-            weight += min(obj.usage, 20)
+                if obj.repo_forks:
+                    weight += min(obj.repo_forks, 20)
 
-        # Is there ongoing work or is this forgotten?
-        if obj.last_committed:
-            if now - obj.last_committed < quarter_delta:
-                weight += 20
-            elif now - obj.last_committed < half_year_delta:
-                weight += 10
-            elif now - obj.last_committed < year_delta:
-                weight += 5
+                if obj.pypi_downloads:
+                    weight += min(obj.pypi_downloads / 1000, 20)
 
-        # Is the last release less than a year old?
-        last_released = obj.last_released
-        if last_released:
-            if now - last_released < year_delta:
-                weight += 20
+                if obj.usage:
+                    weight += min(obj.usage, 20)
 
-        if weight:
-            obj.weight = weight
-            obj.save()
+                # Is there ongoing work or is this forgotten?
+                if obj.last_committed:
+                    if now - obj.last_committed < quarter_delta:
+                        weight += 20
+                    elif now - obj.last_committed < half_year_delta:
+                        weight += 10
+                    elif now - obj.last_committed < year_delta:
+                        weight += 5
 
-        if print_out:
-            print(obj.slug, created, file=stdout)
+                # Is the last release less than a year old?
+                last_released = obj.last_released
+                if last_released:
+                    if now - last_released < year_delta:
+                        weight += 20
+
+                if weight:
+                    obj.weight = weight
+                    obj.save()
+
+                if print_out:
+                    print(obj.slug, created, file=stdout)
 
     print('----------------------', file=stdout)
     max_weight = SearchV2.objects.all()[0].weight
