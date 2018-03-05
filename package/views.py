9be57ec2cda0074d816c2f4edfd915a6b0c17627
Krzysztof Szumny <noisy.pl@gmail.com>
2018-03-05 19:37:43 +0100

    temporarly removing reputation from search result + fix for empty timeline

--- package/views.py
+++ package/views.py
@@ -465,17 +465,21 @@ def package_detail(request, slug, template_name="package/package.html"):
     can_edit_package = hasattr(request.user, "profile") and request.user.profile.can_edit_package(package)
 
     events_on_timeline = 5
-    timeline_events = package.events.order_by('date')
-    timeline_events_reversed = package.events.order_by('-date')
-    timeline_axis_end = timeline_events_reversed.first()
-    timeline_axis_start = timeline_events_reversed[events_on_timeline-1] if timeline_events.count() > events_on_timeline else timeline_events[0]
+    timeline_events = package.events.order_by('-date')
+    timeline_axis_end = timeline_axis_start = None
+
+    if timeline_events.count() > 0:
+        timeline_end = timeline_events.first()
+        timeline_start = timeline_events[events_on_timeline-1] if timeline_events.count() > events_on_timeline else timeline_events[0]
+        timeline_axis_start = timeline_start.date - timedelta(30)
+        timeline_axis_end = timeline_end.date + timedelta(30)
 
     return render(request, template_name,
             dict(
                 package=package,
                 timeline_events=timeline_events,
-                timeline_axis_start=timeline_axis_start.date - timedelta(30),
-                timeline_axis_end=timeline_axis_end.date + timedelta(30),
+                timeline_axis_start=timeline_axis_start,
+                timeline_axis_end=timeline_axis_end,
                 project_imgs=[pi.img for pi in proj_imgs],
                 pypi_ancient=pypi_ancient,
                 no_development=no_development,
