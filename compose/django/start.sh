1458df4f548a7b28e0d43eefe54c3e709314775a
Krzysztof Szumny <noisy.pl@gmail.com>
2018-03-05 04:00:28 +0100

    more workers

--- compose/django/start.sh
+++ compose/django/start.sh
@@ -6,7 +6,7 @@ python /app/manage.py collectstatic --noinput
 /usr/local/bin/uwsgi --http :5000 \
     --wsgi-file /app/wsgi.py \
     --master \
-    --processes 3 \
+    --processes 8 \
     --chdir /app \
     --harakiri 120 \
     --stats :1717 \
