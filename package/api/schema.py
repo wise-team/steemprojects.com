b9b5faf625485a042f5d887ed461f9be6c01c806
Xarv <strzelecki20@gmail.com>
2020-02-19 12:29:38 +0100

    create project and category schema

--- package/api/schema.py
+++ package/api/schema.py
@@ -1,16 +1,36 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from package.models import Category
+from package.models import Category, Project, ProjectImage
 
 
 class CategoryType(DjangoObjectType):
     class Meta:
         model = Category
+        fields = ('title', 'description')
+
+
+class ProjectType(DjangoObjectType):
+    repository_url = graphene.Field(graphene.String)
+
+    class Meta:
+        model = Project
+        fields = ('name', 'description', 'url', 'category', 'images',)
+
+    def resolve_repository_url(self, info):
+        return self.repo_url
+
+
+class ProjectImageType(DjangoObjectType):
+    class Meta:
+        model = ProjectImage
 
 
 class Query(object):
-    all_categories = graphene.List(CategoryType)
+    categories = graphene.List(CategoryType)
+    projects = graphene.List(ProjectType)
 
-    # @graphene.resolve_only_args
-    # def resolve_all_categories(self, info, **kwargs):
-    #     return Category.objects.all()
+    def resolve_categories(self, info, **kwargs):
+        return Category.objects.all()
+
+    def resolve_projects(self, info, **kwargs):
+        return Project.objects.all()
